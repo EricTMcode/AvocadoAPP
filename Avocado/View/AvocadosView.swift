0c412abdcaeb58eb7fa42823db4c09d3c9e3ed67
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-13 14:18:50 +0200

    - Create avocadosView and animate

--- Avocado/View/AvocadosView.swift
+++ Avocado/View/AvocadosView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct AvocadosView: View {
+    @State private var pulsateAnimation = false
+    
     var body: some View {
-        Text("Avocados")
+        VStack {
+            Spacer()
+            Image("avocado")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 240, height: 240, alignment: .center)
+                .shadow(color: .ColorBlackTransparentDark, radius: 12, x: 0, y: 8)
+                .scaleEffect(pulsateAnimation ? 1 : 0.9)
+                .opacity(pulsateAnimation ? 1 : 0.9)
+                .animation(Animation.easeInOut(duration: 1.5).repeatForever(autoreverses: true), value: pulsateAnimation)
+            
+            VStack {
+                Text("Avocados".uppercased())
+                    .font(.system(size: 42, weight: .bold, design: .serif))
+                    .foregroundColor(.white)
+                    .padding()
+                    .shadow(color: .ColorBlackTransparentDark, radius: 4, x: 0, y: 4)
+                
+                Text("""
+Creamy, green, and full of nutrients!
+Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes for breakfast, lunch, dinner & more!
+""")
+                .lineLimit(nil)
+                .font(.system(.headline, design: .serif))
+                .foregroundColor(.ColorGreenLight)
+                .multilineTextAlignment(.center)
+                .lineSpacing(8)
+                .frame(maxWidth: 640, minHeight: 120)
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .background(
+            Image("background")
+                .resizable()
+                .scaledToFill()
+        )
+        .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            pulsateAnimation.toggle()
+        }
     }
 }
 
