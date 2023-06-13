f340c78b98d5c77e29ffe7a8db24430f9d464635
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-13 13:38:29 +0200

    Initial Commit

---
+++ Avocado/AvocadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoApp.swift
+//  Avocado
+//
+//  Created by Eric on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
