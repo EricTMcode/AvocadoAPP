a57c7d618988839a8ac39c69657afd6267d63102
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-13 13:47:31 +0200

    - Create extension on colour to use custom colours

---
+++ Avocado/Utility/Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Extension.swift
+//  Avocado
+//
+//  Created by Eric on 13/06/2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let ColorAppearanceAdaptive = Color("ColorAppearanceAdaptive")
+    static let ColorBlackTransparentDark = Color("ColorBlackTransparentDark")
+    static let ColorBlackTransparentLight = Color("ColorBlackTransparentLight")
+    static let ColorGreenAdaptive = Color("ColorGreenAdaptive")
+    static let ColorGreenDark = Color("ColorGreenDark")
+    static let ColorGreenLight = Color("ColorGreenLight")
+    static let ColorGreenMedium = Color("ColorGreenMedium")
+}
