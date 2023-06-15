abe9eb18647e6ceaced00c64e3b4f76c2ffb7b7c
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-15 13:25:42 +0200

    - Create RecipeDetailView

---
+++ Avocado/View/RecipeDetailView.swift
@@ -0,0 +1,87 @@
+//
+//  RecipeDetailView.swift
+//  Avocado
+//
+//  Created by Eric on 15/06/2023.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    var recipe: Recipe
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 0) {
+                //IMAGE
+                Image(recipe.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                Group {
+                    // TITLE
+                    Text(recipe.title)
+                        .font(.system(.largeTitle, design: .serif))
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.ColorGreenAdaptive)
+                        .padding(.top, 10)
+                    
+                    // RATING
+                    RecipeRatingView(recipe: recipe)
+                    
+                    // COOKING
+                    RecipeCookingView(recipe: recipe)
+                    
+                    //INGREDIENTS
+                    Text("Ingredients")
+                        .bold()
+                        .modifier(TitleModifier())
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        ForEach(recipe.ingredients, id: \.self) { item in
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text(item)
+                                    .font(.footnote)
+                                    .multilineTextAlignment(.leading)
+                                Divider()
+                            }
+                        }
+                    }
+                    
+                    // INSTRUCTIONS
+                    
+                    Text("Instructions")
+                        .bold()
+                        .modifier(TitleModifier())
+                    
+                    ForEach(recipe.instructions, id: \.self) { item in
+                        VStack(alignment: .center, spacing: 5) {
+                            Image(systemName: "chevron.down.circle")
+                                .resizable()
+                                .frame(width: 42, height: 42)
+                                .imageScale(.large)
+                                .font(Font.title.weight(.ultraLight))
+                                .foregroundColor(.ColorGreenAdaptive)
+                            
+                            Text(item)
+                                .lineLimit(nil)
+                                .multilineTextAlignment(.center)
+                                .font(.system(.body, design: .serif))
+                                .frame(minHeight: 100)
+                        }
+                    }
+                }
+                .padding(.horizontal, 24)
+                .padding(.vertical, 12)
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct RecipeDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeDetailView(recipe: recipesData[0])
+    }
+}
